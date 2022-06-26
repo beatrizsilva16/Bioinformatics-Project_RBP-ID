737d0bd534fd837f0dec2f023fa3fbc6dee7cda7
beatr <pg45465@alunos.uminho.pt>
2022-06-26 20:09:55 +0100

    Add code keywords

--- src/blast.py
+++ src/blast.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def run_blast(genome_file, database_name, results_output_file):
+def run_blast(genome_file, database_name, results_output_file,e_value_threshold):
     if "faa" in genome_file:
         blast_type = "blastp"
     elif "fna" in genome_file:
@@ -11,6 +11,8 @@ def run_blast(genome_file, database_name, results_output_file):
 
     process = subprocess.Popen(f"{blast_type} -query {genome_file} "
                                f"-out {results_output_file} -outfmt 5 "
-                               f"-db {database_name} ")
+                               f"-db {database_name} "
+                               f"-evalue {e_value_threshold}", shell=True,
+                               stdout=subprocess.PIPE)
 
     process.wait()
--- test/test.genome.py
+++ test/test.genome.py
@@ -3,4 +3,4 @@ from src.create_db import create_rbps_database
 
 if __name__ == "__main__":
     create_rbps_database("database_rbps.faa", "rbps")
-    run_blast("sequencia_teste.faa", "rbps", "results.xml")
+    run_blast("sequencia_teste.faa", "rbps", "results.xml",1e-20)
