f8318cc561b9b62b42ac4aadfa86024812a0609f
beatr <pg45465@alunos.uminho.pt>
2022-06-26 18:53:44 +0100

    Initial commit

---
+++ test/test.genome.py
@@ -0,0 +1,6 @@
+from src.blast import run_blast
+from src.create_db import create_rbps_database
+
+if __name__ == "__main__":
+    create_rbps_database("database_rbps.faa", "rbps")
+    run_blast("sequencia_teste.faa", "rbps", "results.xml")
