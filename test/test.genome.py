0b54927884d67636594454e076cf291d9d5120a1
beatr <pg45465@alunos.uminho.pt>
2022-06-28 11:45:33 +0100

    blast.phages

--- test/test.genome.py
+++ test/test.genome.py
@@ -1,6 +1,11 @@
-from src.blast import run_blast
-from src.create_db import create_database
+from src.blast_db_RBPs import run_blast
+from src.create_db_RBPs import create_database_RBPs
+from src.file_format_conversion import gb_to_faa
+from blast_db_phages import filter_homologous
 
 if __name__ == "__main__":
-    create_database("database_rbps.faa", "rbps")
-    run_blast("sequencia_teste.faa", "rbps", "results.xml", 1e-20)
+    gb_to_faa("NC_002730.gb.txt", "NC_002730_converted.faa")
+    create_database_RBPs("database_rbps.faa", "rbps")
+    run_blast("NC_002730_converted.faa", "rbps", "results_RBPs.xml", 1e-20)
+    run_blast("NC_002730_converted.faa", "rbps", "results_phages.xml", 1e-20)
+    filter_homologous("results_phages.xml")
