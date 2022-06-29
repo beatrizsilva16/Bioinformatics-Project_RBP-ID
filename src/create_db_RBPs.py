8f858f2b667d0094dfe4523beaace0c7e99e9b94
beatr <pg45465@alunos.uminho.pt>
2022-06-29 23:41:00 +0100

    blast.phages

--- src/create_db_RBPs.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-def create_database_RBPs(db_fasta_file, database_name):
-    process = subprocess.Popen(f" makeblastdb -in {db_fasta_file} -dbtype prot -out {database_name}", shell=True,
-                               stdout=subprocess.PIPE)
-
-    process.wait()
--- test/test.phages.py
+++
@@ -1,6 +0,0 @@
-from src.create_db_phages import create_database_phages
-from src.blast_db_phages import filter_homologous
-
-if __name__ == "__main__":
-    create_database_phages("phage_genomes.txt", "phages")
-    filter_homologous("results_phages.xml")
