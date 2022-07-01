1a039b3e6e9bbef76656c4b88afa42ba9eaa918a
beatrizsilva16 <96452245+beatrizsilva16@users.noreply.github.com>
2022-07-01 02:33:51 +0100

    Add files via upload

---
+++ create_db_phages.py
@@ -0,0 +1,7 @@
+import subprocess
+
+
+def create_database_phages(db_fasta_file, database_name):
+    process = subprocess.Popen(f" makeblastdb -in {db_fasta_file} -dbtype prot -out {database_name}", shell=True,
+                               stdout=subprocess.PIPE)
+    process.wait()
